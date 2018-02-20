2f39e5ac4ebf00d964a81349b8a701895a25548b
Philip Vissenaekens <philipvissenaekens@atmire.com>
2018-02-20 14:41:50 +0100

    49447: spider load synchronization

--- dspace/modules/atmire-statistics-exporter/atmire-statistics-exporter-api/src/main/java/org/dspace/statistics/util/SpiderDetector.java
+++ dspace/modules/atmire-statistics-exporter/atmire-statistics-exporter-api/src/main/java/org/dspace/statistics/util/SpiderDetector.java
@@ -7,16 +7,13 @@
  */
 package org.dspace.statistics.util;
 
-import org.apache.log4j.Logger;
-import org.dspace.core.ConfigurationManager;
-import org.dspace.statistics.SolrLogger;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.regex.*;
+import javax.servlet.http.*;
+import org.apache.log4j.*;
+import org.dspace.core.*;
+import org.dspace.statistics.*;
 
 /**
  * SpiderDetector is used to find IP's that are spiders...
@@ -36,7 +33,7 @@ public class SpiderDetector {
      * Sparse HAshTable structure to hold IP Address Ranges.
      */
     private static IPTable table = null;
-    private static Set<Pattern> spidersRegex = null;
+    private static Set<Pattern> spidersRegex = Collections.synchronizedSet(new HashSet<Pattern>());
     private static Set<String> spidersMatched = null;
 
     /**
@@ -90,7 +87,7 @@ public class SpiderDetector {
         private loader to populate the table from files.
      */
 
-    private static void loadSpiderIpAddresses() {
+    private static synchronized void loadSpiderIpAddresses() {
         if (table == null) {
             table = new IPTable();
 
@@ -206,8 +203,10 @@ public class SpiderDetector {
             log.debug("spider.agentregex");
             return true;
         } else {
-            if (spidersRegex == null)
+            synchronized(spidersRegex) {
+                if (spidersRegex.isEmpty())
                 loadSpiderRegexFromFile();
+            }
 
             if (spidersRegex != null) {
                 for (Object regex : spidersRegex.toArray()) {
@@ -234,7 +233,6 @@ public class SpiderDetector {
      * Original file downloaded from http://www.projectcounter.org/r4/COUNTER_robot_txt_list_Jan_2011.txt during build
      */
     public static void loadSpiderRegexFromFile() {
-        spidersRegex = new HashSet<Pattern>();
         String spidersTxt = ConfigurationManager.getProperty("stats","spider.agentregex.regexfile");
         DataInputStream in = null;
         try {
